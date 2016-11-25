d70e2e2787bcff0e210163933721944b0cbf51eb
42iscool42 <42iscool42@gmail.com>
2016-11-24 22:57:25 -0500

    Fixed issue with repeating-chain and impulse-chain as well as optimized the gutter plugin.

--- gutter.py
+++ gutter.py
@@ -4,72 +4,69 @@ import json
 import os
 
 smeltJsonRegions = []
-codeRegions = []
-codeType = []
+codeLines = {} #A dictionary that matches the blocktype to a list of regions
 
 class UpdateGutterCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		if(self.view.file_name().find("mcc")>0):
-			self.view.add_regions("smeltJson", [], "source")
-			for blockType in ["impulse", "repeating", "chain", "impulse-chain", "repeating-chain"]:
-				self.view.add_regions(blockType, [], "source")
-				self.view.add_regions(blockType + "-conditional", [], "source")
+		self.view.add_regions("smeltJson", [], "source")
+		for blockType in ["impulse", "repeating", "chain", "impulse-chain", "repeating-chain"]:
+			self.view.add_regions(blockType, [], "source")
+			codeLines[blockType] = []
+			self.view.add_regions(blockType + "-conditional", [], "source")
+			codeLines[blockType + "-conditional"] = []
 
-			smeltJsonRegions = self.view.find_all(">\{.*\}")
-			self.view.add_regions("smeltJson",smeltJsonRegions)
-			codeRegions = []
-			codeType = []
-			lastType = "impulse"
-			lastConditional = ""
-			for i in range(len(smeltJsonRegions)):
-				if (i<(len(smeltJsonRegions)-1)):
-					codeRegions.append(sublime.Region(smeltJsonRegions[i].end()+1, smeltJsonRegions[i+1].begin()-1))
-				elif (smeltJsonRegions[i].end()+1<self.view.size()):
-					codeRegions.append(sublime.Region(smeltJsonRegions[i].end()+1, self.view.size()))
+		smeltJsonRegions = self.view.find_all(">\{.*\}")
+		self.view.add_regions("smeltJson",smeltJsonRegions)
+		lastType = "impulse"
+		lastConditional = ""
+		for i in range(len(smeltJsonRegions)):
+			if (i<(len(smeltJsonRegions)-1)):
+				codeRegion = sublime.Region(smeltJsonRegions[i].end()+1, smeltJsonRegions[i+1].begin()-1)
+			elif (smeltJsonRegions[i].end()+1<self.view.size()):
+				codeRegion = sublime.Region(smeltJsonRegions[i].end()+1, self.view.size())
 
-				jsonString = self.view.substr(smeltJsonRegions[i])
-				jsonString = jsonString[jsonString.find("{"):]
-				smeltJson = json.loads(jsonString)
-				newType = ""
-				newConditional = ""
-				try: 
-					conditional = smeltJson["conditional"]
-					if (conditional):
-						newConditional = "-conditional"
-					else:
-						newConditional = ""
-				except KeyError:
-					newConditional = lastConditional
+			jsonString = self.view.substr(smeltJsonRegions[i])
+			jsonString = jsonString[jsonString.find("{"):]
+			smeltJson = json.loads(jsonString)
+			newType = ""
+			newConditional = ""
+			try: 
+				conditional = smeltJson["conditional"]
+				if (conditional):
+					newConditional = "-conditional"
+				else:
+					newConditional = ""
+			except KeyError:
+				newConditional = lastConditional
 
-				try:
-					if (smeltJson["type"] in ["impulse", "repeating", "chain", "impulse-chain", "repeating-chain"]):
-						newType = smeltJson["type"]
-					else:
-						newType = lastType
-				except KeyError:
+			try:
+				if (smeltJson["type"] in ["impulse", "repeating", "chain", "impulse-chain", "repeating-chain"]):
+					newType = smeltJson["type"]
+				else:
 					newType = lastType
+			except KeyError:
+				newType = lastType
 
-				codeType.append(newType+newConditional)
-				for line in self.view.lines(codeRegions[-1]):
-					if(re.search(r"^\s*\/",self.view.substr(line))):
-						regions = []
-						if (self.view.get_regions(newType+newConditional)!=None and len(self.view.get_regions(newType+newConditional)) > 0):
-							regions.extend(self.view.get_regions(newType+newConditional))
-							regions.append(line)
-						else:
-							regions = [line]
-						
-						icon = self.get_icon(newType + newConditional)
-						self.view.add_regions(newType+newConditional, regions, "source",  icon, sublime.PERSISTENT | sublime.HIDDEN)
+			firstLine = True
+			for line in self.view.lines(codeRegion):
+				if(re.search(r"^\s*\/",self.view.substr(line))):
+					if (firstLine and (newType == "impulse-chain" or  newType == "repeating-chain")):
+						firstLine = False
+						newType = newType[0:-6]
+						self.add_code_region(newType+newConditional, line)
+						newType = "chain"
+					else:
+						self.add_code_region(newType+newConditional, line)
 
-					elif (re.search(r"^\s*\#",self.view.substr(line))):
-						newType = "impulse"
-						newConditional = ""
-						lastType = "impulse"
-						lastConditional = ""
 
-				lastType = newType
-				lastConditional = newConditional
+				elif (re.search(r"^\s*\#",self.view.substr(line))):
+					newType = "impulse"
+					newConditional = ""
+					lastType = "impulse"
+					lastConditional = ""
+
+			lastType = newType
+			lastConditional = newConditional
 
 	def get_icon(self, icon):
 		if int(sublime.version())<3014:
@@ -82,6 +79,12 @@ class UpdateGutterCommand(sublime_plugin.TextCommand):
 			extension = ".png"
 		return path + "/icons/" + icon + extension
 
+	def add_code_region(self, label, line):
+		codeLines[label].append(line)
+		
+		icon = self.get_icon(label)
+		self.view.add_regions(label, codeLines[label], "source",  icon, sublime.PERSISTENT | sublime.HIDDEN)
+
 class UpdateGutter(sublime_plugin.EventListener):
 	def on_load(self, view):
 		if (view.file_name().endswith(".mcc")):
@@ -92,9 +95,5 @@ class UpdateGutter(sublime_plugin.EventListener):
 			view.run_command("update_gutter")
 
 	def on_activate(self, view):
-		if (view.file_name().endswith(".mcc")):
-			view.run_command("update_gutter")
-
-	def on_new(self, view):
 		if (view.file_name().endswith(".mcc")):
 			view.run_command("update_gutter")
