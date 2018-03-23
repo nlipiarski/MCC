ebc1903d349a8151463f36c29de5033a42331c40
NateThePlate <42iscool42@gmail.com>
2018-03-22 22:20:42 -0400

    ADded automatic highlighting when editing

--- MccHighlighting.py
+++ MccHighlighting.py
@@ -14,22 +14,30 @@ colorSchemeColors ={
 }
 max_token_ids = {}
 
-class MccHighlightCommand(sublime_plugin.TextCommand):
+class MccHighlightCommand(sublime_plugin.EventListener):
 
-	def run(self, edit):
+	def on_load(self, view):
+		file_name = view.file_name()
+		if file_name != None and len(file_name) > 6 and file_name[-6:] == ".mcc13":
+			self.run(view)
 
-		full_region = sublime.Region(0, self.view.size())
-		file_lines = self.view.lines(full_region)
+	def on_modified(self, view):
+		file_name = view.file_name()
+		if file_name != None and len(file_name) > 6 and file_name[-6:] == ".mcc13":
+			self.run(view)
+
+	def run(self, view):
+
+		full_region = sublime.Region(0, view.size())
+		file_lines = view.lines(full_region)
 		token_id = 0
-		parser = Parser(self.view)
+		parser = Parser(view)
 
 		for line in file_lines:
 			token_id = parser.highlight(COMMAND_TREE, line, 0)
 
-		viewId = self.view.id() # If token_ids aren't overwritten already, remove them from the equation
+		viewId = view.id() # If token_ids aren't overwritten already, remove them from the equation
 		if viewId in max_token_ids and max_token_ids[viewId] > token_id:
 			for i in range(token_id, max_token_ids[viewId]):
-				self.view.add_regions("token" + str(i), [])
+				view.add_regions("token" + str(i), [])
 		max_token_ids[viewId] = token_id
-
-		print("done")
